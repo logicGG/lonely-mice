baf79a2311084f2cadec98dcadbe3bf3bc873b19
黄宏基 <huanghj@gditsec.org.cn>
2023-08-30 15:17:33 +0800

    Initial commit

---
+++ src/main/java/club/ovelya/lonelymice/LonelyMiceApplication.java
@@ -0,0 +1,13 @@
+package club.ovelya.lonelymice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LonelyMiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LonelyMiceApplication.class, args);
+	}
+
+}
